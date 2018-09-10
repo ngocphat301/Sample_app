58ed61fe01b6ab195e72a28301ac8d5779e2706f
ngocpha301 <ngocphat301@gmail.com>
2018-09-10 16:52:03 +0700

    chapter7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
   before_save :downcase_email
   validates :name, presence: true,
-    length: {maximum: Settings.user.name.maximum}
+    length: {maximum: Settings.user.name_maximum}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
-    length: {maximum: Settings.user.name.maximum},
+    length: {maximum: Settings.user.email_maximum},
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
+  validates :password, presence: true, allow_nil: true,
+    length: {minimum: Settings.user.password_minimum}
   has_secure_password
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get "/help", to: "static_pages#help"
   get "/home", to: "static_pages#home"
   get "/about", to: "static_pages#about"
+  get  "/signup",  to: "users#new"
+  resources :users
 end
